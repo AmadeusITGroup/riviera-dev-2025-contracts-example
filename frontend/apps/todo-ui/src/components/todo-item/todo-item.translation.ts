e8f5fb5befa05239e55d2a94b277c34aa234a94b
Matthieu CROUZET <matthieu.crouzet@amadeus.com>
2025-06-06 14:58:47 +0200

    fix: localization for due date

--- frontend/apps/todo-ui/src/components/todo-item/todo-item.translation.ts
+++ frontend/apps/todo-ui/src/components/todo-item/todo-item.translation.ts
@@ -2,6 +2,7 @@ import type { Translation } from '@o3r/core';
 
 export interface TodoItemTranslation extends Translation {
     completedAt: string;
+    dueDate: string;
     toggleDatepickerAxLabel: string;
     assignedToLabel: string;
     toggleStatusLabel: string;
@@ -10,6 +11,7 @@ export interface TodoItemTranslation extends Translation {
 
 export const translations: Readonly<TodoItemTranslation> = {
     completedAt: 'app-todo-item.completedAt',
+    dueDate: 'app-todo-item.dueDate',
     toggleDatepickerAxLabel: 'app-todo-item.toggleDatepickerAxLabel',
     assignedToLabel: 'app-todo-item.assignedToLabel',
     toggleStatusLabel: 'app-todo-item.toggleStatusLabel',
